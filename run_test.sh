61a7be65e4930b6808346515d0e7dc605d07d29c
Mahmudulla Hassan <mhassan@miners.utep.edu>
2019-03-19 16:24:45 -0600

    fix typo

--- run_test.sh
+++ run_test.sh
@@ -1,1 +1,1 @@
-docker run -it --rm --runtime nvidia -v `pwd`:`pwd` -w `pwd` hassanmohsin/dl-docker:gpu python benchmark.py gpu 10000
+docker run -it --rm --runtime=nvidia -v `pwd`:`pwd` -w `pwd` hassanmohsin/dl-docker:gpu python benchmark.py gpu 10000
